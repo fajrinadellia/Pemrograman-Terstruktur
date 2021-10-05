dac234a3b6af890fe07c76dafe614950087a82fc
fajrinadellia <89902081+fajrinadellia@users.noreply.github.com>
2021-10-05 15:12:00 +0700

    Add files via upload

---
+++ Chapter-3/latihan02.py
@@ -0,0 +1,14 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world ")
+print("Hello world ")
+print("Hello world ")
+
+Hello world 
+>>> print("hello world");print("hello world");print("hello world")
+hello world
+hello world
+hello world
+>>> print('hello world', end=' ');print('hello world', end=' ');print('hello world', end=' ')
+hello world hello world hello world 
+>>> 
